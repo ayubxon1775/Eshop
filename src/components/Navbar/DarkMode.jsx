11c06ae2376be4ed4e9414b5b08d80543760f6c5
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-12-10 15:56:13 +0500

    added navbar section

---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LightButton from "../../assets/website/light-mode-button.png";
+import DarkButton from "../../assets/website/dark-mode-button.png";
+
+const DarkMode = () => {
+  const [theme, setTheme] = React.useState("light");
+
+  return (
+   <div>
+    <img src={LightButton} alt="" className= {'w-12 cursor-pointer'} />
+  </div>
+  )
+}
+
+export default DarkMode
