2d9af541097cc7f58339ffea2e471221f1d2f1dc
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-12-15 16:58:50 +0500

    added animated prod

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -45,6 +45,41 @@ const ProductsData = [
   }
   
 ] 
+const ProductsData2 = [
+  {
+    id: 1,
+    img: Img5,
+    title: "Boat Headphone",
+    price: "120",
+    aosDelay: "0",
+
+  },
+  {
+    id: 2,
+    img: Img6,
+    title: "Rocky Mountain",
+    price: "420",
+    aosDelay: "200",
+
+  },
+  {
+    id: 3,
+    img: Img7,
+    title: "Goggles ",
+    price: "320",
+    aosDelay: "400",
+
+  },
+  {
+    id: 4,
+    img: Img4,
+    title: "Printed",
+    price: "220",
+    aosDelay: "600",
+
+  }
+  
+] 
 
 const Products = () => {
   return (
@@ -53,7 +88,8 @@ const Products = () => {
     {/* Heading section */}
     <Heading title="Our products" subtitle={"Explore Our Products"}/>
     {/* Body section */}
-    <ProductCard data={{ProductsData}}/>
+    <ProductCard data={ProductsData}/>
+    <ProductCard data={ProductsData2}/>
       </div>
     </div>
   )
