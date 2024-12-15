02859e7c8d57e31313680404182cff4db2a50a29
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-12-15 09:44:33 +0500

    added Animated banner section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import Hero from './components/Hero/Hero';
 import Category from './components/Category/Category';
 import Category2 from './components/Category/Category2';
 import Services from './components/Services/Services';
+import Banner from './components/Banner/Banner';
+import headphone from './assets/hero/headphone.png'
 
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37"
+}
 const App = () => {
   return (
   <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
@@ -13,6 +25,7 @@ const App = () => {
     <Category/>
     <Category2/>
     <Services/>
+    <Banner data={BannerData}/>
   </div>
   );
 };
