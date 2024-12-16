882159b4cb24e416c8419771871c098b7e2d5f76
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-12-16 18:39:11 +0500

    added reused responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Category2 from './components/Category/Category2';
 import Services from './components/Services/Services';
 import Banner from './components/Banner/Banner';
 import headphone from './assets/hero/headphone.png'
+import smartwatch2 from './assets/Category/smartwatch2-removebg-preview.png'
 import Products from './components/Products/Products';
 
 const BannerData = {
@@ -18,6 +19,16 @@ const BannerData = {
   title4: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
   bgColor: "#f42c37"
 }
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f"
+}
 const App = () => {
   return (
   <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
@@ -28,6 +39,8 @@ const App = () => {
     <Services/>
     <Banner data={BannerData}/>
     <Products/>
+    <Banner data={BannerData2}/>
+
   </div>
   );
 };
