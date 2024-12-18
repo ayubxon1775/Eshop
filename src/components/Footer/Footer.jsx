c36e6bf2318808c4a377140e38f3f71d7aabaca0
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-12-18 13:40:30 +0500

    footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaMobileAlt } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow } from 'react-icons/fa6'
 
 const FooterLinks = [
   {
@@ -30,19 +32,19 @@ const Footer = () => {
              className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl' 
             >Eshop
             </a>
-            <p className='text-gray-600 lg:pr-24 pt-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, perferendis?</p>
+            <p className='text-gray-600 dark:text-white/70 lg:pr-24 pt-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, perferendis?</p>
             <p className='text-gray-500 mt-4'>Made with ❤️ by The Coding Journey</p>
             <a href="https://www.youtube.com/watch?v=4sc6BhgS_Co&t=8550s" target='_blank' className='inline-block bg-primary/90 text-white py-2 px-4 mt-4 text-sm rounded-full'>Visit our YouTube Channel</a>
           </div>
           {/* Footer link */}
           <div className='col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10'>
             <div className='py-8 px-4'>
-              <h1 className='text-xl font-semibold'>Important Link</h1>
+              <h1 className='text-xl font-semibold sm:text-left mb-3'>Important Links</h1>
               <ul className='space-y-3'>
                 {
                   FooterLinks.map((data, index) => (
                     <li key={index}>
-                      <a href={data.link} className='text-gray-600 hover:text-black duration-300'>
+                      <a href={data.link} className='text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300'>
                         {data.title}
                       </a>
                     </li>
@@ -50,6 +52,42 @@ const Footer = () => {
                 }
               </ul>
             </div>
+            {/* second cols link */}
+            <div className='py-8 px-4'>
+              <h1 className='text-xl font-semibold sm:text-left mb-3'>Quick Links</h1>
+              <ul className='space-y-3'>
+                {
+                  FooterLinks.map((data, index) => (
+                    <li key={index}>
+                      <a href={data.link} className='text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300'>
+                        {data.title}
+                      </a>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+                {/* Company Addres */}
+                <div className='py-8 px-4 col-span-2 sm:col-auto'>
+                  <h1 className='text-xl font-bold sm:text-left'>Address</h1>
+                  <div>
+                  <div className="flex items-center gap-3">
+                    <FaLocationArrow/>
+                    <p>Namangan, Namangan shahar</p>
+                    
+                  </div>
+                  <div className='flex items-center gap-3 mt-6'>
+                    <FaMobileAlt/>
+                    <p>+998 99 441 17 75</p>
+                  </div>
+                  {/* social kinks */}
+                  <div className='flex items-center gap-3 mt-6'>
+                    <a href="#"><FaInstagram className='text-3xl hover:text-primary duration-200'/></a>
+                    <a href="#"><FaFacebook className='text-3xl hover:text-primary duration-200'/></a>
+                    <a href="#"><FaLinkedin className='text-3xl hover:text-primary duration-200'/></a>
+                  </div>
+                  </div>
+                </div>
           </div>
         </div>
       </div>
